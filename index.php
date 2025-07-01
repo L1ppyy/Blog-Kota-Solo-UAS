05b560e370b27506981b4659250cdd2de0f680f8
AL <alifiannnsu@gmail.com>
2025-07-01 10:39:45 +0700

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ include 'header.php';
             <img src="assets/batik-solo.jpg" alt="Batik Solo" class="card-img">
             <div class="card-content">
                 <h3 class="card-title">Batik Solo</h3>
-                <p class="card-text">Kota Solo terkenal dengan batiknya yang khas dan bermutu tinggi dengan motif tradisional yang indah.</p>
+                <p class="card-text">Kota Solo terkenal dengan batiknya yang khas dan bermutu tinggi. Setiap motif batik Solo memiliki makna filosofis yang diwariskan turun-temurun, menjadi simbol keharmonisan hidup masyarakat Jawa. Proses pembuatannya pun masih banyak menggunakan teknik tradisional yang sarat nilai seni dan budaya.</p>
                 <a href="budaya.php" class="btn">Selengkapnya</a>
             </div>
         </div>
@@ -28,7 +28,7 @@ include 'header.php';
             <img src="assets/keraton.jpg" alt="Keraton Solo" class="card-img">
             <div class="card-content">
                 <h3 class="card-title">Keraton Surakarta</h3>
-                <p class="card-text">Pusat budaya Jawa yang masih terjaga dengan arsitektur megah dan tradisi yang hidup.</p>
+                <p class="card-text">Pusat budaya Jawa yang masih terjaga keasliannya hingga kini. Keraton Surakarta bukan hanya bangunan bersejarah, tetapi juga saksi perjalanan sejarah kota Solo. Di sini, wisatawan dapat menikmati arsitektur megah, koleksi benda pusaka, serta pertunjukan adat Jawa yang masih rutin digelar.</p>
                 <a href="wisata.php" class="btn">Selengkapnya</a>
             </div>
         </div>
@@ -37,7 +37,7 @@ include 'header.php';
             <img src="assets/sate.jpeg" alt="Sate Solo" class="card-img">
             <div class="card-content">
                 <h3 class="card-title">Kuliner Legendaris</h3>
-                <p class="card-text">Temukan cita rasa autentik kuliner Solo seperti Sate Kambing, Nasi Liwet, dan Tengkleng.</p>
+                <p class="card-text">Pusat budaya Jawa yang masih terjaga keasliannya hingga kini. Keraton Surakarta bukan hanya bangunan bersejarah, tetapi juga saksi perjalanan sejarah kota Solo. Di sini, wisatawan dapat menikmati arsitektur megah, koleksi benda pusaka, serta pertunjukan adat Jawa yang masih rutin digelar.</p>
                 <a href="kuliner.php" class="btn">Selengkapnya</a>
             </div>
         </div>
@@ -50,7 +50,7 @@ include 'header.php';
         <div class="card fade-in">
             <div class="card-content">
                 <h3 class="card-title">Sekaten</h3>
-                <p class="card-text">Festival tahunan untuk memperingati Maulid Nabi Muhammad dengan pasar malam dan pertunjukan gamelan.</p>
+                <p class="card-text">Sekaten adalah festival budaya tahunan yang diadakan untuk memperingati Maulid Nabi Muhammad SAW. Festival ini terkenal dengan pasar malam yang meriah, pertunjukan gamelan pusaka milik keraton, serta prosesi Grebeg Maulud yang membagikan gunungan hasil bumi kepada masyarakat. Sekaten telah menjadi tradisi turun-temurun sejak zaman Kerajaan Demak dan masih lestari di Kota Solo.</p>
                 <p><strong>Waktu:</strong> Setiap bulan Rabiul Awal</p>
             </div>
         </div>
@@ -58,7 +58,7 @@ include 'header.php';
         <div class="card fade-in delay-1">
             <div class="card-content">
                 <h3 class="card-title">Kirab Malam 1 Suro</h3>
-                <p class="card-text">Prosesi budaya mengelilingi kota dengan pusaka keraton untuk menyambut tahun baru Jawa.</p>
+                <p class="card-text">Kirab Malam 1 Suro merupakan prosesi budaya sakral yang dilaksanakan setiap malam tahun baru Jawa. Tradisi ini ditandai dengan kirab pusaka-pusaka keraton, serta keberadaan Kuda Kyai Slamet yang dianggap membawa berkah dan perlindungan. Warga berbondong-bondong mengikuti kirab yang berkeliling kota sebagai simbol doa dan harapan baik di awal tahun Jawa.</p>
                 <p><strong>Waktu:</strong> Malam 1 Suro (Muharram)</p>
             </div>
         </div>
@@ -66,7 +66,7 @@ include 'header.php';
         <div class="card fade-in delay-2">
             <div class="card-content">
                 <h3 class="card-title">Festival Solo Batik Carnival</h3>
-                <p class="card-text">Karnaval busana batik dengan konsep kontemporer yang menampilkan kreativitas desainer muda.</p>
+                <p class="card-text">Solo Batik Carnival (SBC) adalah parade budaya bertema batik yang memadukan seni busana kontemporer dan tradisi batik Nusantara. Peserta karnaval mengenakan kostum spektakuler berbahan batik, lengkap dengan hiasan dan koreografi unik. Event ini rutin digelar di Jalan Slamet Riyadi dan menjadi daya tarik wisata budaya tahunan di Kota Solo.</p>
                 <p><strong>Waktu:</strong> Biasanya bulan Juli</p>
             </div>
         </div>
